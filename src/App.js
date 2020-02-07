70045d56a9eab7639eaedc07824e5172236ed13b
Timdavidcole <timdavidcole@gmail.com>
2020-02-07 17:42:54 +0000

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from "react";
 import Box from "./Components/Box";
-import './Styles/App.css';
+import "./Styles/App.css";
 
 function App() {
   return (
     <div>
-      <h1>
-        Some people drink alcohol because it helps them relax. If you feel the
-        need to relax, try square breathing instead.
-      </h1>
+      <div className="text-box">
+        <span>
+          Some people drink alcohol because it helps them relax. If you feel the
+          need to relax, try square breathing instead.
+        </span>
+      </div>
       <Box />
-      <h3>
-        Square breathing is a simple relaxation technique that promotes
-        measured, mindful breathing. The idea is:
-      </h3>
-      <ul>
-        <li>Breathe in for a slow count of four</li>
-        <li>Hold your breathe for a slow count of four</li>
-        <li>Breathe out for a slow count of four</li>
-        <li>Hold your breathe for a slow count of four</li>
-        <li>Repeat at least four times</li>
-      </ul>
+      <div className="text-box">
+        <span>
+          Square breathing is a simple relaxation technique that promotes
+          measured, mindful breathing. The idea is:
+        </span>
+        <ul>
+          <li>Breathe in for a slow count of four</li>
+          <li>Hold your breathe for a slow count of four</li>
+          <li>Breathe out for a slow count of four</li>
+          <li>Hold your breathe for a slow count of four</li>
+          <li>Repeat at least four times</li>
+        </ul>
+      </div>
     </div>
   );
 }
