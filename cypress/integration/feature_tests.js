a576801f87b04a1a7fc66644eb420de0073e3cf6
timdavidcole <timdavidcole@gmail.com>
2020-02-11 22:37:17 +0000

    added more feature tests

--- cypress/integration/feature_tests.js
+++ cypress/integration/feature_tests.js
@@ -14,8 +14,27 @@ describe("Instructions test", function() {
     cy.visit("http://localhost:3000/");
     cy.contains("Square breathing is a simple relaxation technique that promotes measured, mindful breathing. The idea is:");
   });
-  it("Checks third box has been loaded with correct text", function() {
+  it("Checks the timer box has been loaded with correct text", function() {
     cy.visit("http://localhost:3000/");
     cy.contains("Get ready...");
+  });
+});
+
+describe("Timer test", function() {
+  it("Checks timer box contains instructions", function() {
+    cy.visit("http://localhost:3000/");
+    cy.wait(13000)
+    cy.contains("Breathe in");
+    cy.wait(4000)
+    cy.contains("Hold");
+    cy.wait(4000)
+    cy.contains("Breathe out");
+    cy.wait(4000)
+    cy.contains("Hold");
+  });
+  it("Checks timer box contains stage completed message", function() {
+    cy.visit("http://localhost:3000/");
+    cy.wait(29000)
+    cy.contains("Stage 1 completed");
   });
 });
